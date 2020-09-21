3d876f09a6c8267d483678b8a8231488248e7b65
Paulo Santos <paulo@adianta.com.br>
2020-09-20 21:06:38 -0300

    Adicionando teste criação de produtos sem criar endpoint - barra vermelha

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 
 from sw_api.database import session
@@ -24,3 +26,21 @@ def test_get_products(tst, fixture):
     assert response.status_code == 200
     assert len(response.json) == fixture.total_products
     assert set(response.json[0].keys()) == {'id', 'name', 'value'}
+
+
+def test_post_product_success(tst):
+    assert session().query(Product).count() == 0
+    # Em uma aplicação com autenticação seria necessário envio de token
+    payload = {
+        'name': 'Novo Produto',
+        'value': 340.5
+    }
+    response = tst.client.post('/products', headers={'Content-Type': 'application/json'}, data=payload)
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json == {
+        'id': 1,
+        'name': 'Novo Produto',
+        'value': 340.5
+    }
+
